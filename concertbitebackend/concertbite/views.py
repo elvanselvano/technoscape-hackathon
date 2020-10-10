cce5b76e24a15486380111529ef60589beedfaaa
admantiumblack <felixroland12355@gmail.com>
2020-10-10 17:06:01 +0700

    added api

--- concertbitebackend/concertbite/views.py
+++ concertbitebackend/concertbite/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from .models import User
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+def getUserDetail(request, username, password):
+    users = User.objects.filter(username=username,
+                                password=password).values('Id', 'firstName', 'lastName', 'email', 'password')
+    users = list(users)
+    return JsonResponse(users, safe = False)
