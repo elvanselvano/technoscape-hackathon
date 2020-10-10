822ace2545f5cc0ac3a702bc7f7d2bb587079886
admantiumblack <felixroland12355@gmail.com>
2020-10-10 17:08:19 +0700

    Update views.py

--- concertbitebackend/concertbite/views.py
+++ concertbitebackend/concertbite/views.py
@@ -4,10 +4,11 @@ from rest_framework import viewsets
 from .serializers import UserSerializer
 from .models import User
 from rest_framework.response import Response
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
 
-
+@require_http_methods(['POST'])
 def getUserDetail(request, username, password):
     users = User.objects.filter(username=username,
                                 password=password).values('Id', 'firstName', 'lastName', 'email', 'password')
